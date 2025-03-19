e39e4429d6c2822fa7668559db9f952a1265341f
MBP <fx.colombani@pcagrade.fr>
2025-03-19 12:15:26 +0100

    pokimages version 1.5

--- src/main/java/com/pca/pokimages/controller/CardController.java
+++ src/main/java/com/pca/pokimages/controller/CardController.java
@@ -1,43 +1,109 @@
 package com.pca.pokimages.controller;
 
-// src/main/java/com/example/pokimages/controller/CardController.java
-
-import com.pca.pokimages.client.PokeApiClient;
-import com.pca.pokimages.model.Card;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/cards")
 public class CardController {
 
-    @Autowired
-    private PokeApiClient pokeApiClient;
-
-
-
-    @GetMapping("/{id}")
-    public String getCardById(@PathVariable String id) {
-        return pokeApiClient.getPokemonById(id); // Retourne JSON brut pour l'instant
-    }
-
+    private final RestTemplate restTemplate = new RestTemplate();
+    private static final String POKEMON_TCG_API = "https://api.pokemontcg.io/v2/cards";
 
     @GetMapping
     public List<Card> getAllCards() {
-        // Exemple statique pour tester
-        List<Card> cards = new ArrayList<>();
-        cards.add(new Card("1", "Bulbasaur", "https://pokeapi.co/media/sprites/1.png"));
-        return cards;
+        PokemonTcgResponse response = restTemplate.getForObject(POKEMON_TCG_API + "?q=name:bulbasaur", PokemonTcgResponse.class);
+        if (response == null || response.getData() == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Aucune carte trouvée");
+        }
+        return response.getData();
     }
 
-    @GetMapping("/{id}/image")
-    public String getCardImage(@PathVariable String id) {
-        // Simuler une URL d'image
-        return "https://pokeapi.co/media/sprites/" + id + ".png";
+    @GetMapping("/{id}")
+    public Card getCardById(@PathVariable String id) {
+        PokemonTcgCardResponse response = restTemplate.getForObject(POKEMON_TCG_API + "/" + id, PokemonTcgCardResponse.class);
+        if (response == null || response.getData() == null) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Carte non trouvée : " + id);
+        }
+        return response.getData();
+    }
+}
+
+class PokemonTcgResponse {
+    private List<Card> data;
+
+    public List<Card> getData() {
+        return data;
     }
 
+    public void setData(List<Card> data) {
+        this.data = data;
+    }
+}
 
+class PokemonTcgCardResponse {
+    private Card data;
+
+    public Card getData() {
+        return data;
+    }
+
+    public void setData(Card data) {
+        this.data = data;
+    }
+}
+
+class Card {
+    private String id;
+    private String name;
+    private Images images;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Images getImages() {
+        return images;
+    }
+
+    public void setImages(Images images) {
+        this.images = images;
+    }
+}
+
+class Images {
+    private String small;
+    private String large;
+
+    public String getSmall() {
+        return small;
+    }
+
+    public void setSmall(String small) {
+        this.small = small;
+    }
+
+    public String getLarge() {
+        return large;
+    }
+
+    public void setLarge(String large) {
+        this.large = large;
+    }
 }
